2b4e967d1800cd2ffb1507f4392be8a86a689f94
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-03-01 09:44:03 +0100

    ci(gitlab): Add MongoDB 8.2.5 in the CI

--- .gitlab-ci.main.kts
+++ .gitlab-ci.main.kts
@@ -1,5 +1,21 @@
 #!/usr/bin/env kotlin
 
+/*
+ * Copyright (c) 2026, OpenSavvy and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // https://gitlab-ci-kts.opensavvy.dev/news/index.html
 @file:DependsOn("dev.opensavvy.gitlab:gitlab-ci-kotlin-jvm:0.7.2")
 
@@ -108,6 +124,10 @@ gitlabCi {
 
 	val checkJvm by job(stage = test) {
 		jvm()
+
+		service("mongo", "8.2.5") {
+			alias = "mongo"
+		}
 
 		script {
 			gradlew.tasks(
